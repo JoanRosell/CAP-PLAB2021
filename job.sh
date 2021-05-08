e2ef408eadabbc1b0a3c57bc9cb4b82f74504c06
Joan Rosell <joan.rosellr@gmail.com>
2021-05-08 13:38:59 +0200

    chore: fix TAU tracing execution

--- job.sh
+++ job.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #SBATCH --exclusive
-# This script compiles and executes the project
+# This script compiles and executes the project in two steps:
+#   1. Compile and run the project using TAU to trace the execution
+#   2. Compile and run the project without TAU
+#
 # Arguments (listed in read order):
 #   (int) epochs: number of training iterations
 #   (int) numIn: number of input neurons
@@ -15,20 +18,6 @@ module load cmake/3.13.4
 module load openmpi/3.0.0
 module load tau/2.29
 
-# TAU exports
-export TAU_MAKEFILE=/soft/tau-2.29/x86_64/lib/Makefile.tau-mpi
-export TAU_OPTIONS=-optCompInst
-#export TAU_TRACE=1
-
-# Select the compilation variable for CMake
-export CC=$(which tau_cc.sh)
-
-# Remove the old build system
-rm -rf build/* 
-
-# Compile using CMake
-./compile.sh
-
 # Parse parameters
 filename=$1
 epochs=$2
@@ -36,11 +25,36 @@ numIn=$3
 numHid=$4
 numOut=$5
 
-# Execute
-mpirun ./build/CAP-PLAB2021.exe $epochs $numIn $numHid $numOut
+# Tracing constants
+TRACE_EPOCHS=5
 
-# Process the trace files
-#tau_treemerge.pl
-#tau2slog2 tau.trc tau.edf -o tau.slog2
-pprof
+# TAU exports
+export TAU_MAKEFILE=/soft/tau-2.29/x86_64/lib/Makefile.tau-mpi
+export TAU_OPTIONS=-optCompInst
+export TAU_TRACE=1
+
+# 1. Traced version:
+# Select the compilation variable for CMake, in this case use the TAU wrappers
+export CC=$(which tau_cc.sh)
+
+# Remove the old build system
+rm -rf build/* 
+
+# Compile, run and process the output files
+./compile.sh
+mpirun ./build/CAP-PLAB2021.exe $TRACE_EPOCHS $numIn $numHid $numOut
+tau_treemerge.pl
+tau2slog2 tau.trc tau.edf -o $filename.slog2
+
+
+# 2. Non-traced version
+# Select the compilation variable for CMake, in this case use the standard MPI wrappers
+export CC=$(which mpicc)
+
+# Remove the old build system
+rm -rf build/* 
+
+# Compile and run
+./compile.sh
+mpirun ./build/CAP-PLAB2021.exe $epochs $numIn $numHid $numOut
 
